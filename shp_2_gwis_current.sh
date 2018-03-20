b4a4f695b458531c1b7c08ce5cab2ea805069247
Fabio <t_arch@hotmail.com>
2018-03-20 12:47:04 +0100

    terminato anche con rilevazione anomalie

--- shp_2_gwis_current.sh
+++ shp_2_gwis_current.sh
@@ -8,8 +8,15 @@ filetype="*.shp"
 
 PSQL=/usr/bin/psql
 DB_HOST=localhost
-DB_NAME=test_egeos
+DB_NAME=effis
+DB_SCHEMA=egeos
 DB_USER=postgres
+
+TABLE_PROCESSED_FILES=processed_data
+TABLE_CURRENT_BA=current_ba
+TABLE_EVOLUTION_BA=evolution_ba
+TABLE_TEMPORARY_UPLOAD=temp_ba
+TABLE_ANOMALIES=ba_problems
 
 #RUN_ON_DB="$PSQL -X -U $DB_USER -h $DB_HOST --set ON_ERROR_STOP=on --set AUTOCOMMIT=off $DB_NAME"
 #already_in=$($PSQL -X -U $DB_USER -h $DB_HOST -P t -P format=unaligned $DB_NAME -c "SELECT COUNT(shp_id) FROM processed_data WHERE filename='$fname'")
@@ -30,19 +37,22 @@ do
           ./get_contained_ba.sh $nomefile 
     else        
         
-        already_in=$($RUN_ON_DB -c "SELECT COUNT(shp_id) FROM processed_data WHERE filename='$fname'")
+        already_in=$($RUN_ON_DB -c "SELECT COUNT(shp_id) FROM $DB_SCHEMA.$TABLE_PROCESSED_FILES WHERE filename='$fname'")
 
         if [ $already_in == 0 ]; then
-            file_date=$(echo $fbname | awk -F'_' '{print $1}')
-            sensor=$(echo $fbname | awk -F'_' '{print $2}')
-            data_type=$(echo $fbname | awk -F'_' '{print $3}')
+            
+             file_date=$(echo $fbname | awk -F'_' '{print $1}')
+             sensor=$(echo $fbname | awk -F'_' '{print $2}')
+             data_type=$(echo $fbname | awk -F'_' '{print $3}')
              
-            echo "file date: $file_date, sensor: $sensot, data_type: $data_type"               
+             echo $fname 
+             echo "file date: $file_date, sensor: $sensor, data_type: $data_type"               
               
-            $RUN_ON_DB -c "INSERT INTO processed_data(filename,data_date,data_type,sensor) \
-                           VALUES('$fname',to_date('$file_date','YYYYMMDD'),'$sensor','$data_type')" 
+             $RUN_ON_DB -c "INSERT INTO $DB_SCHEMA.$TABLE_PROCESSED_FILES(filename,data_date,data_type,sensor) \
+                            VALUES('$fname',to_date('$file_date','YYYYMMDD'),'$sensor','$data_type')" 
 
-            shp2pgsql -a -s 4326 -g geom $nomefile temp_ba | psql -U postgres -d test_egeos
+             shp2pgsql -a -s 4326 -g geom $nomefile $DB_SCHEMA.$TABLE_TEMPORARY_UPLOAD | psql -U $DB_USER -d $DB_NAME
+
         else
             echo "$fname already processed. Cannot be repocessed with the same filename"
         fi
@@ -50,49 +60,43 @@ do
 done
 }
 
+
 truncate_tables(){
-    lista_tabelle=( temp_ba current_ba processed_data evolution_ba )  
+    lista_tabelle=( $DB_SCHEMA.$TABLE_TEMPORARY_UPLOAD 
+                    $DB_SCHEMA.$TABLE_CURRENT_BA 
+                    $DB_SCHEMA.$TABLE_PROCESSED_FILES 
+                    $DB_SCHEMA.$TABLE_EVOLUTION_BA 
+                    $DB_SCHEMA.$TABLE_ANOMALIES )  
+    
     for tabella in "${lista_tabelle[@]}"
     do
        $RUN_ON_DB -c "TRUNCATE $tabella" 
     done
 }
 
-truncate_table(){
-    $RUN_ON_DB -c "TRUNCATE $1" 
-}
 
 check_id_current_ba(){
 
-CURRENT_ID=$($RUN_ON_DB -c "select max(id) from current_ba")
-let NEXT_ID=CURRENT_ID+1
+     CURRENT_ID=$($RUN_ON_DB -c "select max(id) from  $DB_SCHEMA.$TABLE_CURRENT_BA")
+     let NEXT_ID=CURRENT_ID+1
 
-echo "Next id for burnt areas is $NEXT_ID"
+     echo "Next id for burnt areas is $NEXT_ID"
 
 }
 
 current_bas(){
 
-set -e
-set -u
-
-$PSQL \
-     -X \
-     -U $DB_USER \
-     -h $DB_HOST \
-     -c "select id, firedate, area_ha from current_ba" \
-     --single-transaction \
-     --set AUTOCOMMIT=off \
-     --set ON_ERROR_STOP=on \
-     --no-align \
-     -t \
-     --field-separator ' ' \
-     --quiet \
-     -d $DB_NAME \ | while read -a Record; do
-      
-     id=${Record[0]}
-     firedate=${Record[1]}
-     area_ha=${Record[2]}
+$RUN_ON_DB -c "select id, firedate, area_ha from $DB_SCHEMA.$TABLE_CURRENT_BA" \
+           --single-transaction \
+           --set AUTOCOMMIT=off \
+           --set ON_ERROR_STOP=on \
+           --no-align \
+           --field-separator ' ' \
+           --quiet \ | while read -a Record; do
+             
+        id=${Record[0]}
+        firedate=${Record[1]}
+        area_ha=${Record[2]}
                         
      echo "id: $id, firedate: $firedate, area_hectares: $area_ha"
  done
@@ -101,19 +105,13 @@ $PSQL \
 
 current_evolution_bas(){
 
-$PSQL \
-     -X \
-     -U $DB_USER \
-     -h $DB_HOST \
-     -c "select * from link_current_evolution();" \
-     --single-transaction \
-     --set AUTOCOMMIT=off \
-     --set ON_ERROR_STOP=on \
-     --no-align \
-     -t \
-     --field-separator ' ' \
-     --quiet \
-     -d $DB_NAME \ | while read -a Record; do
+$RUN_ON_DB -c "select * from egeos.link_current_evolution();" \
+           --single-transaction \
+           --set AUTOCOMMIT=off \
+           --set ON_ERROR_STOP=on \
+           --no-align \
+           --field-separator ' ' \
+           --quiet \ | while read -a Record; do
 
      id=${Record[0]}
      updated_firedate=${Record[1]}
@@ -128,38 +126,50 @@ $PSQL \
 
 no_evolution_bas(){
 
-$PSQL \
-     -X \
-     -U $DB_USER \
-     -h $DB_HOST \
-     -c "select * from link_current_no_evolution();" \
-     --single-transaction \
-     --set AUTOCOMMIT=off \
-     --set ON_ERROR_STOP=on \
-     --no-align \
-     -t \
-     --field-separator ' ' \
-     --quiet \
-     -d $DB_NAME \ | while read -a Record; do
+$RUN_ON_DB -c "select * from egeos.link_current_no_evolution();" \
+           --single-transaction \
+           --set AUTOCOMMIT=off \
+           --set ON_ERROR_STOP=on \
+           --no-align \
+           --field-separator ' ' \
+           --quiet \ | while read -a Record; do
 
      id=${Record[0]}     
      initial_firedate=${Record[1]}
-     initial_area_ha=${Record[2]}
-     
+     initial_area_ha=${Record[2]}     
                         
      echo "fire id: $id, initial date: $initial_firedate, initial area: $initial_area_ha"
   done
 }
 
 
+ba_anomalies(){
+
+$RUN_ON_DB -c "select * from egeos.ba_problems;" \
+           --single-transaction \
+           --set AUTOCOMMIT=off \
+           --set ON_ERROR_STOP=on \
+           --no-align \
+           --field-separator ' ' \
+           --quiet \ | while read -a Record; do
+
+     sequence=${Record[0]}     
+     ba_id=${Record[1]}
+     anomaly_definition=${Record[2]}     
+                        
+     echo "sequence id: $sequence, ba_id: $ba_id, anomaly type: $anomaly_definition"
+  done
+}
+
+
 declare -a choices=("Shapefile Metadata")
-#choices[${#choices[*]}]="Shapefile Metadata" 
 choices[${#choices[*]}]="Current Burnt Areas"
 choices[${#choices[*]}]="Evolution Burnt Areas"
 choices[${#choices[*]}]="One Day Burnt Areas No Evolution" 
 choices[${#choices[*]}]="Empty Tables"
 choices[${#choices[*]}]="Upload Shape"
 choices[${#choices[*]}]="Last ID Current BurntArea"
+choices[${#choices[*]}]="BurntArea Anomalies"
 choices[${#choices[*]}]="List of Options"
 choices[${#choices[*]}]="Quit"
 #choices+=( "Current Burnt Areas" "Empty Tables" "Upload Shape" "Quit" )
@@ -184,8 +194,10 @@ do
         ${choices[5]})
             manage_shapefiles 2 ;;
         ${choices[6]})
-           check_id_current_ba ;;
+            ba_anomalies ;;
         ${choices[7]})
+           check_id_current_ba ;;
+        ${choices[8]})
              break ;;
         *) echo "Exiting...."
             exit ;;
